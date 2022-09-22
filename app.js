343b3fa0e6b88fe9b30415efacf7f911f39aabd8
pvsaint <vpeter817@gmail.com>
2022-09-22 01:10:11 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const result = document.getElementById('result');
+const checkBtn = document.getElementById('checkBtn');
+const cityInput = document.getElementById('cityInput');
+const cityForm = document.getElementById('cityForm');
+
+let apiRequest = new XMLHttpRequest();
+
+cityForm.addEventListener('submit', ($event) => {
+    $event.preventDefault();
+    const chosenCity = cityInput.value;
+    apiRequest.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q=' + chosenCity + 
+    '&APPID=b34fddd3dae4a2eb0ad363b62f98ba1e');
+    apiRequest.send();
+});
+
+apiRequest.onreadystatechange = () => {
+    if (apiRequest.readyState === 4) {
+        if (apiRequest.status === 404) {
+            return result.textContent = 'City not found!';
+        }
+        const response = JSON.parse(apiRequest.response);
+        result.textContent = 'The weather in ' + response.name + ' is ' + response.weather[0].main + '.';
+    }
+}
